a57fad8b5d5574f68ba7c50a9b3b40fa832ac818
sergeyeliseev <s.eliseev1@gmail.com>
2020-10-29 18:19:24 +0300

    deleted

---
+++ src/ru/job4j/Task42.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Task42 {
+    public static int func1 (int x) {
+        int k = x + 1;
+        return k;
+    }
+    public static void main (String[] args) {
+        int result = Task42. func1(100);
+        System.out.println (result);
+    }
+
+}
