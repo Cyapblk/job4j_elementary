615bfa0d0ee6276c5429bb404bc2fd932e48f9ba
sergeyeliseev <s.eliseev1@gmail.com>
2020-10-29 22:45:27 +0300

    0. Что такое тестирование [#396499] float to int

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -1,29 +1,29 @@
 package ru.job4j;
 
 public class Converter {
-    public static float rubleToEuro(float value) {
-        float rsl = value / 70;
+    public static int rubleToEuro(int value) {
+        int rsl = value / 70;
         return rsl;
     }
 
-    public static float rubleToDollar(float value) {
-        float rsl = value / 60;
+    public static int rubleToDollar(int value) {
+        int rsl = value / 60;
         return rsl;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(140);
+        int euro = Converter.rubleToEuro(140);
+        int dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("140 rubles are " + dollar + " dollar.");
         int in = 140;
-        float expeur = 2;
-        float expusd = 2.3333333F;
-        float outeur = Converter.rubleToEuro(in);
-        float outusd = Converter.rubleToDollar(in);
+        int expeur = 2;
+        int expusd = 2;
+        int outeur = Converter.rubleToEuro(in);
+        int outusd = Converter.rubleToDollar(in);
         boolean passedeur = expeur == outeur;
         boolean passedusd = expusd == outusd;
         System.out.println("140 rubles are 2 euro. Test result : " + passedeur);
-        System.out.println("140 rubles are 2.3333333 dollar Test result : " + passedusd);
+        System.out.println("140 rubles are 2 dollar Test result : " + passedusd);
     }
 }
