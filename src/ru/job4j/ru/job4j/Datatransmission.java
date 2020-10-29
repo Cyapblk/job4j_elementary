147dc1135c1409175c8ba37c64e1e063ff9421f8
sergeyeliseev <s.eliseev1@gmail.com>
2020-10-29 21:36:27 +0300

    Datatransmission

---
+++ src/ru/job4j/ru/job4j/Datatransmission.java
@@ -0,0 +1,11 @@
+package ru.job4j.ru.job4j;
+
+public class Datatransmission {
+    public static void main(String[] args) {
+        short s = 1500;
+        char c = (char) s;
+        double d = c;
+
+    System.out.println("Вывод в консоль значения после преобразования: " + d);
+}
+    }
