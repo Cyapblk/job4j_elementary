5fb293cd3186bb250934cc16c417cea25c1635e9
sergeyeliseev <s.eliseev1@gmail.com>
2020-10-31 13:24:18 +0300

    1.1. Входные данные в тесте [#396505]

--- test/ru/job4j/calculate/X2Test.java
+++ test/ru/job4j/calculate/X2Test.java
@@ -31,9 +31,9 @@ public class X2Test {
 
     @Test
     public void whenA1B1C0X1Then2() {
-        int a = 0;
+        int a = 1;
         int b = 1;
-        int c = 1;
+        int c = 0;
         int x = 1;
         int exp = 2;
         int rsl = X2.calc(a, b, c, x);
@@ -42,11 +42,11 @@ public class X2Test {
 
     @Test
     public void whenA1B1C1X0Then1() {
-        int a = 0;
+        int a = 1;
         int b = 1;
         int c = 1;
-        int x = 1;
-        int exp = 2;
+        int x = 0;
+        int exp = 1;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(exp, rsl);
     }
