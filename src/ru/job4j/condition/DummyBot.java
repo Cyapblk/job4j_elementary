281e6cc3598ed40f561b026049b37ad3203a50d6
sergeyeliseev <s.eliseev1@gmail.com>
2020-11-04 21:16:32 +0300

    just updates

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -1,15 +1,13 @@
 package ru.job4j.condition;
 
 public class DummyBot {
-
     public static String answer(String question) {
-        String rsl = "какая разница что сюда написать?.";
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник.";
-        } else if ("Пока.".equals(question)) {
+        }
+        if ("Пока.".equals(question)) {
             rsl = "До скорой встречи";
-        } else {
-            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         }
         return rsl;
     }
