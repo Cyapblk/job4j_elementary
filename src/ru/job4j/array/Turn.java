e0755f5332a6bc915d1a1d0493a65e66fdfc2bf1
sergeyeliseev <s.eliseev1@gmail.com>
2020-11-17 00:14:20 +0300

    6.2.2. Переставить элементы массива [#396453]

---
+++ src/ru/job4j/array/Turn.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+        if (array.length % 2 == 0) {
+            for (int index = 0; index <= (array.length - 1) / 2; index++) {
+                int temp = array[index];
+                array[index] = array[array.length - 1 - index];
+                array[array.length - 1 - index] = temp; }
+            } else {
+                for (int index = 0; index <= (array.length) / 2; index++) {
+                    int temp = array[index];
+                    array[index] = array[array.length - 1 - index];
+                    array[array.length - 1 - index] = temp;
+                }
+            }
+            return array;
+        }
+    }
