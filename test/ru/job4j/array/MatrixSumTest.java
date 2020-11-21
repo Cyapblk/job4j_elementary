a3767676cb6bb2cde5d18bf85d504bf65fc43f62
sergeyeliseev <s.eliseev1@gmail.com>
2020-11-21 20:39:08 +0300

    6.6.1. Двухмерный массив. Вывод на консоль. [#396454]

---
+++ test/ru/job4j/array/MatrixSumTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MatrixSumTest {
+
+    @Test
+    public void sumZero() {
+        int[][] input = {
+                {1, 2, 1},
+                {1, 2, 333},
+                {11, 12, -363}
+        };
+        Assert.assertEquals(MatrixSum.sum(input), 0);
+        }
+
+    @Test
+    public void sumZeroagain() {
+        int[][] input = {
+                {-777, -1, -2},
+                {-3, -4, 4},
+                {3, 3, 777}
+        };
+        Assert.assertEquals(MatrixSum.sum(input), 0);
+    }
+}
