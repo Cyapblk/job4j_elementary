33c32ba00b045b62e3651b52511b5d7b8d036c55
sergeyeliseev <s.eliseev1@gmail.com>
2020-12-04 19:45:38 +0300

    Merge ver. 666

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -1,36 +1,32 @@
 package ru.job4j.array;
 
 public class Merge {
-    public class Merge {
-        public static int[] merge(int[] left, int[] right) {
-            int[] rsl = new int[left.length + right.length];
-            int counter = 0;
-        while (counter < rsl.length) {
-            int lft = 0;
-            int rght = 0;
-            if (left[lft] < right[rght] && left[lft + 1] < right[rght + 1]) {
-                rsl[counter] = left[lft];
+    public static int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int a = 0;
+        int b = 0;
+        int counter = 0;
+        int rl = right.length;
+        while (a < left.length && b < right.length) {
+            if (left[a] < right[b]) {
+                rsl[counter] = left[a];
                 counter++;
-                lft++;
-            }
-
-            while (left[lft] > right[rght] && left[lft + 1] > right[rght + 1]) { // 2
-                rsl[counter] = right[rght];
+                a++;
+            } else {
+                rsl[counter] = right[b];
                 counter++;
-                rght++;
+                b++;
             }
-
-            while (left[lft] > right[rght] && left[lft + 1] < right[rght + 1]) { //
-                rsl[counter] = right[rght];
-                counter++;
-                lft++;
-            }
-
-            while (left[lft] < right[rght] && left[lft + 1] > right[rght + 1]) { //
-                rsl[counter] = left[lft];
-                counter++;
-                rght++;
-            }
+        }
+        while (a < left.length) {
+            rsl[counter] = left[a];
+            counter++;
+            a++;
+        }
+        while (b < right.length) {
+            rsl[counter] = right[b];
+            counter++;
+            b++;
         }
         return rsl;
     }
